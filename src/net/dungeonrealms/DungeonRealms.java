bb5dd20e4379eeb18d83091ae9a9e2ef5599e50a
ifSwitch <nick@nicks.co>
2015-09-17 18:07:51 -0400

    main push

---
+++ src/net/dungeonrealms/DungeonRealms.java
@@ -0,0 +1,24 @@
+package net.dungeonrealms;
+
+import net.dungeonrealms.mastery.Utils;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Nick on 9/17/2015.
+ */
+public class DungeonRealms extends JavaPlugin {
+
+
+    public void onLoad() {
+        Utils.log.info("DungeonRealms onLoad() ... STARTING UP");
+    }
+
+    public void onEnable() {
+        Utils.log.info("DungeonRealms onEnable() ... STARTING UP");
+    }
+
+    public void onDisable() {
+        Utils.log.info("DungeonRealms onDisable() ... SHUTTING DOWN");
+    }
+
+}
---
+++ src/net/dungeonrealms/mastery/Utils.java
@@ -0,0 +1,12 @@
+package net.dungeonrealms.mastery;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by Nick on 9/17/2015.
+ */
+public class Utils {
+
+    public static Logger log = Logger.getLogger("DungeonRealms");
+
+}
