414fe70ddd8dd1e2920e68de12fc634bb60ca663
ifSwitch <nick@nicks.co>
2015-09-18 01:12:46 -0400

    +buffs were working then broke ;/

--- src/net/dungeonrealms/commands/CommandSpawn.java
+++ src/net/dungeonrealms/commands/CommandSpawn.java
@@ -1,17 +1,16 @@
 package net.dungeonrealms.commands;
 
 import net.dungeonrealms.mastery.NBTUtils;
-import net.dungeonrealms.monsters.entities.EntityPirate;
-import net.minecraft.server.v1_8_R3.World;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
-import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
+import org.bukkit.entity.EnderCrystal;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Wolf;
+import org.bukkit.potion.PotionEffectType;
 
 /**
  * Created by Nick on 9/17/2015.
@@ -28,11 +27,9 @@ public class CommandSpawn implements CommandExecutor {
                     Wolf w = (Wolf) Bukkit.getWorld(player.getWorld().getName()).spawnEntity(player.getLocation(), EntityType.WOLF);
                     NBTUtils.nullifyAI(w);
                     break;
-                case "pirate":
-                    World world = ((CraftWorld) player.getWorld()).getHandle();
-                    EntityPirate zombie = new EntityPirate(world);
-                    world.addEntity(zombie);
-                    zombie.setPosition(player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ());
+                case "buff":
+                    EnderCrystal ec = (EnderCrystal) Bukkit.getWorld(player.getWorld().getName()).spawnEntity(player.getLocation(), EntityType.ENDER_CRYSTAL);
+                    NBTUtils.buffEntity(ec, PotionEffectType.REGENERATION, 6, 20 * 10);
                     break;
                 default:
             }
