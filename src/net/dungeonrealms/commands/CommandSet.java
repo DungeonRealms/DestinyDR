556604fa278b0597026b4c1c96327944568a9d8a
Xwaffle1 <chasecool56@gmail.com>
2015-09-23 00:48:45 -0400

    ItemSerialization

--- src/net/dungeonrealms/commands/CommandSet.java
+++ src/net/dungeonrealms/commands/CommandSet.java
@@ -3,12 +3,27 @@
  */
 package net.dungeonrealms.commands;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
+import net.dungeonrealms.DungeonRealms;
+import net.dungeonrealms.mastery.ItemSerialization;
 import net.dungeonrealms.mongo.DatabaseAPI;
 import net.dungeonrealms.mongo.EnumOperators;
 
@@ -25,13 +40,77 @@ public class CommandSet implements CommandExecutor {
 		if (args.length > 0) {
 			switch (args[0]) {
 			case "level":
-				int lvl = Integer.parseInt(args[1]);
-				DatabaseAPI.getInstance().update(player.getUniqueId(), EnumOperators.$SET, "info.netLevel", lvl);
-				s.sendMessage("Level set to " + lvl);
+			int lvl = Integer.parseInt(args[1]);
+			DatabaseAPI.getInstance().update(player.getUniqueId(), EnumOperators.$SET, "info.netLevel", lvl);
+			s.sendMessage("Level set to " + lvl);
 			case "gems":
-				int gems = Integer.parseInt(args[1]);
-				DatabaseAPI.getInstance().update(player.getUniqueId(), EnumOperators.$SET, "info.gems", gems);
-				s.sendMessage("Gems set to " + gems);
+			int gems = Integer.parseInt(args[1]);
+			DatabaseAPI.getInstance().update(player.getUniqueId(), EnumOperators.$SET, "info.gems", gems);
+			s.sendMessage("Gems set to " + gems);
+			case "save":
+			if (player.getItemInHand() != null) {
+				ArrayList<ItemStack> items = new ArrayList<>();
+				items.add(player.getItemInHand());
+
+				InputStream inputStream = null;
+				OutputStream outputStream = null;
+
+				try {
+					File file = new File(DungeonRealms.getInstance().getDataFolder() + "//file.json");
+					file.createNewFile();
+					inputStream = ItemSerialization.serialize(items);
+					// read this file into InputStream
+					outputStream = new FileOutputStream(file);
+
+					int read = 0;
+					byte[] bytes = new byte[1024];
+
+					while ((read = inputStream.read(bytes)) != -1) {
+						outputStream.write(bytes, 0, read);
+					}
+
+					System.out.println("Done!");
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				} finally {
+					player.setItemInHand(null);
+					if (inputStream != null) {
+						try {
+						inputStream.close();
+						} catch (IOException e) {
+						e.printStackTrace();
+						}
+					}
+					if (outputStream != null) {
+						try {
+						// outputStream.flush();
+						outputStream.close();
+						} catch (IOException e) {
+						e.printStackTrace();
+						}
+
+					}
+				}
+
+			}
+			case "load":
+			File file = new File(DungeonRealms.getInstance().getDataFolder() + "//file.json");
+			try {
+				InputStream inputStream = new FileInputStream(file);
+				List<ItemStack> items = ItemSerialization.deserialize(inputStream);
+				for (int i = 0; i < items.size(); i++) {
+					if (items.get(i) != null) {
+						player.getInventory().addItem(items.get(i));
+					}
+				}
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			}
 		}
 		return true;
---
+++ src/net/dungeonrealms/mastery/ItemSerialization.java
@@ -0,0 +1,56 @@
+package net.dungeonrealms.mastery;
+
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import net.minecraft.server.v1_8_R3.PacketDataSerializer;
+
+public class ItemSerialization {
+
+	public static InputStream serialize(Collection<ItemStack> items) throws IOException {
+		ByteArrayInputStream ret = null;
+		if (!items.isEmpty()) {
+			ByteBuf buf = Unpooled.buffer();
+			PacketDataSerializer serializer = new PacketDataSerializer(buf);
+			buf.writeInt(items.size());
+			for (ItemStack item : items)
+			serializer.a(CraftItemStack.asNMSCopy(item));
+			ret = new ByteArrayInputStream(serializer.array());
+		}
+		return ret;
+	}
+
+	public static List<ItemStack> deserialize(InputStream input) throws IOException {
+		List<ItemStack> items = new ArrayList<>();
+		DataInputStream in = new DataInputStream(input);
+		ByteBuf buf = Unpooled.buffer();
+		buf.writeBytes(IOUtils.toByteArray(in));
+		PacketDataSerializer serializer = new PacketDataSerializer(buf);
+		try {
+			int count = buf.readInt();
+			for (int i = 0; i <= count; i++) {
+			net.minecraft.server.v1_8_R3.ItemStack item = serializer.i();
+			if (item != null) {
+				items.add(CraftItemStack.asCraftMirror(item));
+			}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (items.size() == 0)
+			items.add(new ItemStack(Material.AIR));
+		return items;
+	}
+}
