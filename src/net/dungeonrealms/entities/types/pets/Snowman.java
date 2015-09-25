49a8f6ab276c26e15bdebc163612507a5d9907a6
Proxying <kieran_quigley_CFC@hotmail.com>
2015-09-25 19:03:54 +0100

    CHRISTMAS

---
+++ src/net/dungeonrealms/entities/types/pets/Snowman.java
@@ -0,0 +1,35 @@
+package net.dungeonrealms.entities.types.pets;
+
+import net.dungeonrealms.enums.EnumEntityType;
+import net.dungeonrealms.mastery.MetadataUtils;
+import net.minecraft.server.v1_8_R3.EntitySnowman;
+import net.minecraft.server.v1_8_R3.World;
+
+import java.util.UUID;
+
+/**
+ * Created by Kieran on 9/25/2015.
+ */
+public class Snowman extends EntitySnowman {
+
+    private String mobName;
+    private UUID ownerUUID;
+    private EnumEntityType entityType;
+
+    public Snowman(World world, String mobName, UUID ownerUUID, EnumEntityType entityType) {
+        super(world);
+        this.mobName = mobName;
+        this.ownerUUID = ownerUUID;
+        this.entityType = entityType;
+        this.getBukkitEntity().setCustomNameVisible(true);
+        this.getBukkitEntity().setCustomName(mobName);
+        this.canPickUpLoot = false;
+        this.persistent = true;
+
+        MetadataUtils.registerEntityMetadata(this, this.entityType, 0, 0);
+    }
+
+    public Snowman(World world) {
+        super(world);
+    }
+}
