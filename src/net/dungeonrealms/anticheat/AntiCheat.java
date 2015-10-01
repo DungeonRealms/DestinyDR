ae57521ebeec3009088fce8be0f687ef71df4205
ifSwitch <nick@nicks.co>
2015-10-01 18:00:19 -0400

    AntiCheat

--- src/net/dungeonrealms/anticheat/AntiCheat.java
+++ src/net/dungeonrealms/anticheat/AntiCheat.java
@@ -1,5 +1,7 @@
 package net.dungeonrealms.anticheat;
 
+import net.dungeonrealms.mastery.AsyncUtils;
+import net.dungeonrealms.mastery.Utils;
 import net.minecraft.server.v1_8_R3.NBTTagCompound;
 import net.minecraft.server.v1_8_R3.NBTTagString;
 import org.bukkit.Material;
@@ -8,18 +10,80 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.ItemStack;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 /**
  * Created by Nick on 10/1/2015.
  */
-public class AntiDupe {
+public class AntiCheat {
 
-    static AntiDupe instance = null;
+    static AntiCheat instance = null;
 
-    public static AntiDupe getInstance() {
+    public static AntiCheat getInstance() {
         if (instance == null) {
-            instance = new AntiDupe();
+            instance = new AntiCheat();
         }
         return instance;
+    }
+
+    /**
+     * Because Kayaba and Red are going to be such bitches.
+     * ANd we all know that negros like them will follow
+     * the path of niggerish nigger. We'll build in a automagically
+     * proxyifier detector to detect dem negro from 2 farm fields
+     * away.
+     *
+     * @param uuid
+     * @param ip
+     * @return
+     * @since 1.0
+     */
+    public boolean isProxying(UUID uuid, InetAddress ip) {
+
+        Future<Boolean> isProxy = AsyncUtils.pool.submit(() -> {
+            Utils.log.info("[ANTI-PROXY] [ASYNC] Checking player " + uuid.toString() + " w/ ip " + ip.toString().replace("/", ""));
+            try {
+
+                URL url = new URL("http://www.shroomery.org/ythan/proxycheck.php?ip=" + ip.toString().replace("/", ""));
+                URLConnection connection = url.openConnection();
+                connection.connect();
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine = in.readLine();
+                in.close();
+                if (inputLine == null) return false;
+                switch (inputLine) {
+                    case "Y":
+                        return true;
+                    case "N":
+                        return false;
+                    case "X":
+                        Utils.log.warning("Unable to check ifIs Proxy for user " + uuid);
+                        return false;
+                    default:
+                        Utils.log.warning("DEFAULT FIRED SWITCH() " + uuid);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return false;
+        });
+
+        try {
+            return isProxy.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     /**
@@ -30,7 +94,7 @@ public class AntiDupe {
      * @return
      * @since 1.0
      */
-    public boolean clickEvent(InventoryClickEvent event) {
+    public boolean watchForDupes(InventoryClickEvent event) {
         if (event.getInventory().getType() != null && event.getInventory().getType() == InventoryType.CREATIVE)
             return false;
         ItemStack checkItem = event.getCursor();
