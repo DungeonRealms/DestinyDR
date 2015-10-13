05459c1accba6dc25c6f679fc3e32f2ed6b18461
Xwaffle1 <chasecool56@gmail.com>
2015-10-13 12:31:33 -0400

    Removed Hearthsteone from players inventory added to Player Profile.

--- src/net/dungeonrealms/mechanics/PlayerManager.java
+++ src/net/dungeonrealms/mechanics/PlayerManager.java
@@ -21,9 +21,6 @@ public class PlayerManager {
      */
     public static void checkInventory(UUID uuid) {
         Player player = Bukkit.getPlayer(uuid);
-        player.getInventory().setItem(7, ItemManager.createHearthStone("HearthStone", new String[]{
-                ChatColor.AQUA + "Teleport home to " + ChatColor.YELLOW + ChatColor.BOLD + TeleportAPI.getLocationFromDatabase(uuid).toUpperCase() + ChatColor.RESET + ChatColor.AQUA + "!",
-        }));
         player.getInventory().setItem(8, ItemManager.getPlayerProfile(player, "Player Profile", new String[]{
                 ChatColor.GRAY + "(Right-Click) " + ChatColor.AQUA + "Open your profile!"
         }));
