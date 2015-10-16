08d1b1e2f1b25e2f16c4ca5a9e6b1970204f1587
ifSwitch <nick@nicks.co>
2015-10-15 23:08:30 -0400

    fixed minor error

--- src/net/dungeonrealms/network/NetworkAPI.java
+++ src/net/dungeonrealms/network/NetworkAPI.java
@@ -7,7 +7,7 @@ import com.google.common.io.ByteStreams;
 import net.dungeonrealms.API;
 import net.dungeonrealms.DungeonRealms;
 import net.dungeonrealms.guild.Guild;
-import net.dungeonrealms.mail.Mail;
+import net.dungeonrealms.handlers.MailHandler;
 import net.dungeonrealms.mastery.Utils;
 import net.dungeonrealms.mongo.DatabaseAPI;
 import net.dungeonrealms.mongo.EnumGuildData;
@@ -52,7 +52,7 @@ public class NetworkAPI implements PluginMessageListener {
                 if (in.readUTF().equals("update")) {
                     Bukkit.getOnlinePlayers().stream().filter(p -> p.getName().equals(in.readUTF())).forEach(p -> {
                         DatabaseAPI.getInstance().requestPlayer(p.getUniqueId());
-                        Mail.getInstance().sendMailMessage(p, ChatColor.GREEN + "You got mail!");
+                        MailHandler.getInstance().sendMailMessage(p, ChatColor.GREEN + "You got mail!");
                     });
                 }
                 break;
