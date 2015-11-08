d34b0eb6aa023e8e75b790b27faa9076805093c3
Proxying <kieran_quigley_CFC@hotmail.com>
2015-11-08 03:59:03 +0000

    Buff stuff.

--- src/net/dungeonrealms/entities/utils/BuffUtils.java
+++ src/net/dungeonrealms/entities/utils/BuffUtils.java
@@ -5,7 +5,6 @@ import net.dungeonrealms.entities.types.EnderCrystal;
 import net.dungeonrealms.mastery.MetadataUtils;
 import net.minecraft.server.v1_8_R3.World;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
@@ -31,13 +30,10 @@ public class BuffUtils {
         Player player = Bukkit.getPlayer(uuid);
         World world = ((CraftWorld) player.getWorld()).getHandle();
         EnderCrystal enderCrystal = new EnderCrystal(world, EnumEntityType.BUFF);
-        enderCrystal.setLocation(player.getLocation().getX() + new Random().nextInt(10), player.getLocation().getY() + 1, player.getLocation().getZ() + new Random().nextInt(10), 0, 0);
+        Block b = Bukkit.getWorlds().get(0).getHighestBlockAt((int) player.getLocation().getX(), (int) player.getLocation().getZ());
+        enderCrystal.setLocation(b.getX(), b.getY(), b.getZ(), 0, 0);
         world.addEntity(enderCrystal, CreatureSpawnEvent.SpawnReason.CUSTOM);
-        enderCrystal.setLocation(player.getLocation().getX() + new Random().nextInt(10), player.getLocation().getY() + 1, player.getLocation().getZ() + new Random().nextInt(10), 0, 0);
-        if (enderCrystal.getBukkitEntity().getLocation().subtract(0, 1, 0).getBlock().getType() == Material.AIR) {
-            Block b = Bukkit.getWorlds().get(0).getHighestBlockAt((int) player.getLocation().getX(), (int) player.getLocation().getZ());
-            enderCrystal.setLocation(b.getX(), b.getY(), b.getZ(), 0, 0);
-        }
+        enderCrystal.setLocation(b.getX(), b.getY(), b.getZ(), 0, 0);
         player.playSound(player.getLocation(), Sound.ENDERDRAGON_WINGS, 1f, 63f);
         MetadataUtils.registerBuffMetadata(enderCrystal, getRandomPotionEffect(), 10, 600);
         return enderCrystal;
