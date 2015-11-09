5a6fddd03cd369a72dfe0062aba265deeab57425
ifSwitch <nick@nicks.co>
2015-11-09 00:33:47 -0500

    fix

--- src/net/dungeonrealms/world/glyph/Glyph.java
+++ src/net/dungeonrealms/world/glyph/Glyph.java
@@ -326,7 +326,7 @@ public class Glyph {
             }
         }
 
-
+        itemTag.set("bound", new NBTTagString("true"));
         nmsStack.setTag(itemTag);
 
         ItemStack stack = CraftItemStack.asBukkitCopy(nmsStack);
