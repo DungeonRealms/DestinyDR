64b20e7aa7d9c079416b264ed4668e22f2b49963
Xwaffle1 <chasecool56@gmail.com>
2015-11-09 14:38:46 -0500

    Lowered armor/weapon drop rate for monster.

--- src/net/dungeonrealms/entities/types/monsters/Monster.java
+++ src/net/dungeonrealms/entities/types/monsters/Monster.java
@@ -53,10 +53,10 @@ public interface Monster {
 			}
 			
 			item.setAmount(amount);
-			world.getWorld().dropItemNaturally(loc.add(0, 1, 0), item);
+			world.getWorld().dropItemNaturally(loc.add(0, 2, 0), item);
 		}
 
-		if (RandomHelper.getRandomNumberBetween(1, 100) <= 10) {
+		if (RandomHelper.getRandomNumberBetween(1, 100) <= 5) {
 			ItemStack[] loot = new ItemStack[5];
 			ItemStack[] armor = ((LivingEntity) ent).getEquipment().getArmorContents();
 			ItemStack weapon = ((LivingEntity) ent).getEquipment().getItemInHand();
@@ -64,7 +64,7 @@ public interface Monster {
 			loot = new ItemStack[]{armor[0], armor[1], armor[2], armor[3], weapon};
 			int number = RandomHelper.getRandomNumberBetween(0, 4);
 			ItemStack armorToDrop = loot[number];
-			world.getWorld().dropItemNaturally(loc.add(0, 1, 0), armorToDrop);
+			world.getWorld().dropItemNaturally(loc.add(0, 2, 0), armorToDrop);
 		}
 	}
 }
