0b6b437b3ee27c96e37aa18db7ce5c4375135a61
Xwaffle1 <chasecool56@gmail.com>
2015-11-10 08:38:04 -0500

    Bank Upgrade prices fixed.

--- src/net/dungeonrealms/listeners/BankListener.java
+++ src/net/dungeonrealms/listeners/BankListener.java
@@ -224,7 +224,7 @@ public class BankListener implements Listener {
                         } else if (e.isRightClick()) {
                             Inventory inv = Bukkit.createInventory(null, 9, "Upgrade your bank storage?");
                         	int invLvl = (int) DatabaseAPI.getInstance().getData(EnumData.INVENTORY_LEVEL, player.getUniqueId());
-                			int num = invLvl * 100;
+                			int num = BankMechanics.getPrice(invLvl);
                             ItemStack accept = new ItemStack(Material.WOOL, 1, DyeColor.LIME.getData());
                             ItemMeta acceptMeta = accept.getItemMeta();
                             acceptMeta.setDisplayName(ChatColor.GREEN.toString() +"Accept");
