4e7be0534cc09b2e4e50ccc081f0bdf20641cd00
Xwaffle1 <chasecool56@gmail.com>
2015-12-01 12:51:58 -0500

    Food Vendor and change exp cap to +20 levels

--- src/net/dungeonrealms/game/world/shops/ShopMechanics.java
+++ src/net/dungeonrealms/game/world/shops/ShopMechanics.java
@@ -1,5 +1,6 @@
 package net.dungeonrealms.game.world.shops;
 
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -7,6 +8,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -20,6 +22,7 @@ import net.dungeonrealms.game.mechanics.generic.GenericMechanic;
 import net.dungeonrealms.game.mongo.DatabaseAPI;
 import net.dungeonrealms.game.mongo.EnumData;
 import net.dungeonrealms.game.mongo.EnumOperators;
+import net.dungeonrealms.game.player.inventory.NPCMenus;
 
 /**
  * Created by Chase on Nov 17, 2015
@@ -126,5 +129,21 @@ public class ShopMechanics implements GenericMechanic{
 	@Override
 	public void stopInvocation() {
       deleteAllShops();
+	}
+	
+	/**
+	 * @param item 
+	 * @param price
+	 * @return
+	 */
+	public static ItemStack addPrice(ItemStack item, int price) {
+		ItemMeta meta = item.getItemMeta();
+		List<String> lore = meta.getLore();
+		lore.add(ChatColor.GREEN + "Price: " + ChatColor.WHITE + price + "g");
+        String[] arr = lore.toArray(new String[lore.size()]);
+        item = NPCMenus.editItem(item, item.getItemMeta().getDisplayName(), arr);
+        net.minecraft.server.v1_8_R3.ItemStack nms = CraftItemStack.asNMSCopy(item);
+        nms.getTag().setInt("worth", price);
+		return CraftItemStack.asBukkitCopy(nms);
 	}
 }
