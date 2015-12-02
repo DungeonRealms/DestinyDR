219e465d9d44b9ede10326bab052830938c49b6b
Xwaffle1 <chasecool56@gmail.com>
2015-12-01 21:22:55 -0500

    Check combat logger, logout command.

--- src/net/dungeonrealms/game/commands/CommandLogout.java
+++ src/net/dungeonrealms/game/commands/CommandLogout.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Player;
 import net.dungeonrealms.API;
 import net.dungeonrealms.DungeonRealms;
 import net.dungeonrealms.game.commands.generic.BasicCommand;
+import net.dungeonrealms.game.player.combat.CombatLog;
 import net.md_5.bungee.api.ChatColor;
 
 /**
@@ -22,6 +23,12 @@ public class CommandLogout extends BasicCommand {
 	public boolean onCommand(CommandSender s, Command cmd, String string, String[] args) {
 		if (s instanceof Player) {
 			Player player = (Player) s;
+			
+			if(CombatLog.isInCombat(player)){
+				player.sendMessage(ChatColor.RED + "You can not use /logout while in combat.");
+				return false;
+			}
+			
 			player.sendMessage(ChatColor.GREEN + ChatColor.BOLD.toString() + "Starting Logout...");
 			API.handleLogout(player.getUniqueId());
 			Bukkit.getScheduler().scheduleSyncDelayedTask(DungeonRealms.getInstance(), ()-> player.kickPlayer(ChatColor.GREEN + "Safely Logged Out!"), 100);
