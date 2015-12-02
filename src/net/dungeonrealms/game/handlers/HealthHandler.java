b70556e44a04b60861e9e0f9034c851f1d655f3f
Proxying <kieran_quigley_CFC@hotmail.com>
2015-12-02 23:41:34 +0000

    commit to merge

--- src/net/dungeonrealms/game/handlers/HealthHandler.java
+++ src/net/dungeonrealms/game/handlers/HealthHandler.java
@@ -51,7 +51,7 @@ public class HealthHandler implements GenericMechanic {
     }
 
     public void startInitialization() {
-        Bukkit.getScheduler().runTaskTimer(DungeonRealms.getInstance(), this::updatePlayerHPBars, 40, 8L);
+        Bukkit.getScheduler().runTaskTimer(DungeonRealms.getInstance(), this::updatePlayerHPBars, 40, 10L);
         Bukkit.getScheduler().runTaskTimer(DungeonRealms.getInstance(), this::regenerateHealth, 40, 20L);
     }
 
@@ -84,7 +84,7 @@ public class HealthHandler implements GenericMechanic {
             }
             setPlayerHPRegenLive(player, getPlayerHPRegenLive(player));
             player.setMetadata("last_death_time", new FixedMetadataValue(DungeonRealms.getInstance(), System.currentTimeMillis()));
-        }, 50L);
+        }, 60L);
     }
 
     /**
