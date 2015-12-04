053cebeef3d81abc724d778369cbec2b4fcc6d4d
Proxying <kieran_quigley_CFC@hotmail.com>
2015-12-04 03:52:54 +0000

    test

--- src/net/dungeonrealms/DungeonRealms.java
+++ src/net/dungeonrealms/DungeonRealms.java
@@ -210,7 +210,20 @@ public class DungeonRealms extends JavaPlugin {
 
         Bukkit.getScheduler().scheduleAsyncDelayedTask(DungeonRealms.getInstance(), () -> {
             Bukkit.broadcastMessage(ChatColor.YELLOW + ChatColor.BOLD.toString() + "WARNING: " + ChatColor.RED + "A SCHEDULED  " + ChatColor.BOLD + "REBOOT" + ChatColor.RED + " WILL TAKE PLACE IN 5 MINUTES");
-            Bukkit.getScheduler().scheduleAsyncDelayedTask(DungeonRealms.getInstance(), Bukkit::shutdown, 6000);
+            Bukkit.getScheduler().scheduleSyncDelayedTask(DungeonRealms.getInstance(), () -> {
+                DungeonRealms.getInstance().setFinishedSetup(false);
+                DungeonRealms.getInstance().saveConfig();
+                ShopMechanics.deleteAllShops();
+                API.logoutAllPlayers();
+                Bukkit.getScheduler().scheduleSyncDelayedTask(DungeonRealms.getInstance(), () -> {
+                    DungeonRealms.getInstance().mm.stopInvocation();
+                    Utils.log.info("DungeonRealms onDisable() ... SHUTTING DOWN");
+                    AsyncUtils.pool.shutdown();
+                    Database.mongoClient.close();
+                    Bukkit.getWorlds().get(0).save();
+                }, 40);
+                Bukkit.getScheduler().scheduleSyncDelayedTask(DungeonRealms.getInstance(), Bukkit::shutdown, 100);
+            }, 6000);
         }, 288000);
 
 
