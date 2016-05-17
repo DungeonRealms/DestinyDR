cbfacfa1529f40d17791c134fdda7aac155568bf
Kayaba <Kayaba@yourfunworld.com>
2016-05-17 14:42:01 -0400

    Identify to the master server with the bungee name

--- src/net/dungeonrealms/game/network/handlers/PacketHandler.java
+++ src/net/dungeonrealms/game/network/handlers/PacketHandler.java
@@ -2,7 +2,9 @@ package net.dungeonrealms.game.network.handlers;
 
 import org.bukkit.Bukkit;
 
+import net.dungeonrealms.DungeonRealms;
 import net.dungeonrealms.game.mastery.Utils;
+import net.dungeonrealms.game.network.NetworkServer;
 import net.dungeonrealms.game.network.packets.BroadcastPacket;
 import net.dungeonrealms.game.network.packets.PartyPacket;
 
@@ -10,6 +12,14 @@ public class PacketHandler {
 
 	public static void handlePacket(Object obj)
 	{
+    	if(obj instanceof String)
+    	{
+    		String RAW = (String)obj;
+    		if(RAW.contains("@"))
+    		{
+    			NetworkServer.getInstance().client.getServerConnection().sendTcp("@" + DungeonRealms.getInstance().bungeeName);
+    		}
+    	}
 		if(obj instanceof BroadcastPacket)
 		{
 			BroadcastPacket packet = (BroadcastPacket)obj;
