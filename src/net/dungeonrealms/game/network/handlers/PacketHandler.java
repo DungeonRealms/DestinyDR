e76041074506b7f83c2642bf546781697fc08559
Kayaba <Kayaba@yourfunworld.com>
2016-05-17 14:23:08 -0400

    Moving packet check to Master server side of things

--- src/net/dungeonrealms/game/network/handlers/PacketHandler.java
+++ src/net/dungeonrealms/game/network/handlers/PacketHandler.java
@@ -2,7 +2,6 @@ package net.dungeonrealms.game.network.handlers;
 
 import org.bukkit.Bukkit;
 
-import net.dungeonrealms.DungeonRealms;
 import net.dungeonrealms.game.mastery.Utils;
 import net.dungeonrealms.game.network.packets.BroadcastPacket;
 import net.dungeonrealms.game.network.packets.PartyPacket;
@@ -14,20 +13,12 @@ public class PacketHandler {
 		if(obj instanceof BroadcastPacket)
 		{
 			BroadcastPacket packet = (BroadcastPacket)obj;
-			if(packet.serverid.contains(DungeonRealms.getInstance().bungeeName))
-			{
-				Bukkit.broadcastMessage(packet.message);
-				return;
-			}
+			Bukkit.broadcastMessage(packet.message);
+			return;
 		}
 		else if(obj instanceof PartyPacket)
 		{
-			PartyPacket packet = (PartyPacket)obj;
-			if(packet.getTo().equals(DungeonRealms.getInstance().bungeeName))
-			{
-				// Handle the Party packet here... 
-				return;
-			}
+			return;
 		}
 		else {
 			Utils.log.warning("Recieved a packet that the server couldn't handle!");
