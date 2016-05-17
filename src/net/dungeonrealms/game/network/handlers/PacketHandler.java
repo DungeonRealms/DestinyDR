6883b46dae627a95a63676afd0201527d8a28b83
Kayaba <Kayaba@yourfunworld.com>
2016-05-17 14:18:38 -0400

    Added PartyPacket handler

--- src/net/dungeonrealms/game/network/handlers/PacketHandler.java
+++ src/net/dungeonrealms/game/network/handlers/PacketHandler.java
@@ -5,6 +5,7 @@ import org.bukkit.Bukkit;
 import net.dungeonrealms.DungeonRealms;
 import net.dungeonrealms.game.mastery.Utils;
 import net.dungeonrealms.game.network.packets.BroadcastPacket;
+import net.dungeonrealms.game.network.packets.PartyPacket;
 
 public class PacketHandler {
 
@@ -16,6 +17,15 @@ public class PacketHandler {
 			if(packet.serverid.contains(DungeonRealms.getInstance().bungeeName))
 			{
 				Bukkit.broadcastMessage(packet.message);
+				return;
+			}
+		}
+		else if(obj instanceof PartyPacket)
+		{
+			PartyPacket packet = (PartyPacket)obj;
+			if(packet.getTo().equals(DungeonRealms.getInstance().bungeeName))
+			{
+				// Handle the Party packet here... 
 				return;
 			}
 		}
