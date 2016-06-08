f9b238128e5003dbd8294a38af315ad4b285e0f3
Brad <brad.bayliss@blueyonder.co.uk>
2016-06-08 23:37:30 +0100

    Fix rolling to match old DR.

--- src/main/java/net/dungeonrealms/game/commands/CommandRoll.java
+++ src/main/java/net/dungeonrealms/game/commands/CommandRoll.java
@@ -6,6 +6,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import net.dungeonrealms.game.player.chat.GameChat;
 
 import java.util.Random;
 
@@ -25,24 +26,26 @@ public class CommandRoll extends BasicCommand {
             return false;
         }
         if (args.length != 1) {
-            sender.sendMessage(ChatColor.RED + "Incorrect syntax. /Roll <1-10000>");
+            sender.sendMessage(ChatColor.RED + "Incorrect syntax. /roll <1-10000>");
             return false;
         }
 
         try {
             int max = Integer.parseInt(args[0]);
             if (max < 1 || max > 10000) {
-                sender.sendMessage(ChatColor.RED + "Incorrect syntax. /Roll <1-10000>");
+                sender.sendMessage(ChatColor.RED + "Incorrect syntax. /roll <1-10000>");
                 return false;
             }
             Player player = (Player) sender;
 
             int roll = new Random().nextInt(max) + 1;
 
-            API.getNearbyPlayers(player.getLocation(), 20).stream().forEach(player1 -> player1.sendMessage(ChatColor.AQUA.toString() + ChatColor.UNDERLINE + player.getName() +
-                    ChatColor.GRAY + " has rolled a " + ChatColor.RED + roll + ChatColor.GRAY + " out of " + ChatColor.RED + max + ChatColor.GRAY + "."));
+            String playerName = GameChat.getPreMessage(player, false, "local");
+
+            API.getNearbyPlayers(player.getLocation(), 20).stream().forEach(player1 -> player1.sendMessage(playerName.substring(0, playerName.length() - 4) +
+                    ChatColor.GRAY + " has rolled a " + ChatColor.UNDERLINE + ChatColor.BOLD + roll + ChatColor.GRAY + " out of " + ChatColor.UNDERLINE + ChatColor.BOLD + max + ChatColor.GRAY + "."));
         } catch (Exception ex) {
-            sender.sendMessage(ChatColor.RED + "Incorrect syntax. /Roll <1-10000>");
+            sender.sendMessage(ChatColor.RED + "Incorrect syntax. /roll <1-10000>");
             return false;
         }
 
