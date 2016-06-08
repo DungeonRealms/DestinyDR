3ea67fd7dd01323bd577348192c9fae37ba1cac9
apollosoftware <contact@apollosoftware.io>
2016-06-07 20:59:36 -0400

    Removed Copyright

--- src/main/java/net/dungeonrealms/tool/UpdateDevServerApplication.java
+++ src/main/java/net/dungeonrealms/tool/UpdateDevServerApplication.java
@@ -3,15 +3,6 @@ package net.dungeonrealms.tool;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-/**
- * Copyright © 2016 APOLLOSOFTWARE.IO
- * All rights reserved. No part of this publication may be reproduced, distributed, or
- * transmitted in any form or by any means, including photocopying, recording, or other
- * electronic or mechanical methods, without the prior written permission of the publisher,
- * except in the case of brief quotations embodied in critical reviews and certain other
- * noncommercial uses permitted by copyright law.
- */
-
 public class UpdateDevServerApplication {
 
     static String TOOL_PATH = "\"C:\\Users\\XenoJava\\Desktop\\DR\\DungeonRealms\\tools\\pushBuildToServer\"";
