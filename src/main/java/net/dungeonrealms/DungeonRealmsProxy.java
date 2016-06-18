50ca95a31b86117fafbfec6ecb08720774d96db6
apollosoftware <contact@apollosoftware.io>
2016-06-18 12:51:58 -0400

    Created Bungee Load Balancer

--- src/main/java/net/dungeonrealms/DungeonRealmsProxy.java
+++ src/main/java/net/dungeonrealms/DungeonRealmsProxy.java
@@ -5,25 +5,31 @@ import com.mongodb.MongoClientURI;
 import net.dungeonrealms.game.guild.GuildDatabaseAPI;
 import net.dungeonrealms.game.guild.db.GuildDatabase;
 import net.dungeonrealms.game.listeners.ProxyChannelListener;
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.ServerConnectEvent;
+import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.event.EventHandler;
 import org.bson.Document;
 
-import java.util.UUID;
+import java.util.*;
 
 /**
  * Class written by APOLLOSOFTWARE.IO on 5/31/2016
  */
 
-public class DungeonRealmsProxy extends Plugin {
+public class DungeonRealmsProxy extends Plugin implements Listener {
 
     private static DungeonRealmsProxy instance;
     public static com.mongodb.MongoClient mongoClient = null;
     public static MongoClientURI mongoClientURI = null;
     public static com.mongodb.client.MongoDatabase database = null;
     public static com.mongodb.client.MongoCollection<Document> guilds = null;
+
+    private final String[] DR_SHARDS = new String[]{"dr1", "dr2"};
 
     @Override
     public void onEnable() {
@@ -40,6 +46,36 @@ public class DungeonRealmsProxy extends Plugin {
 
         getLogger().info("DungeonRealms [MONGODB] has connected successfully!");
         this.getProxy().getPluginManager().registerListener(this, ProxyChannelListener.getInstance());
+        this.getProxy().getPluginManager().registerListener(this, this);
+    }
+
+    public List<ServerInfo> getOptimalShards() {
+        List<ServerInfo> server = new ArrayList<>();
+
+        for (String shardName : DR_SHARDS) server.add(getProxy().getServerInfo(shardName));
+        Collections.sort(server, (o1, o2) -> o1.getPlayers().size() - o2.getPlayers().size());
+
+        return server;
+    }
+
+    @EventHandler
+    public void onServerConnect(ServerConnectEvent event) {
+        if ((event.getPlayer().getServer() == null) || event.getTarget().getName().equals("Lobby")) {
+            Iterator<ServerInfo> optimalShardFinder = getOptimalShards().iterator();
+
+            event.getPlayer().sendMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "Finding an available shard for you...");
+
+            optimalShardFinder.forEachRemaining(target -> {
+                if (target.canAccess(event.getPlayer()) && !(event.getPlayer().getServer() != null && event.getPlayer().getServer().getInfo().equals(target))) {
+                    try {
+                        event.setTarget(target);
+                    } catch (Exception e) {
+                        if (!optimalShardFinder.hasNext())
+                            event.getPlayer().disconnect(ChatColor.RED + "Could not find an optimal shard for you.. Please try again later.");
+                    }
+                }
+            });
+        }
     }
 
     public void sendMessageToGuild(String guildName, String message, String... filters) {
