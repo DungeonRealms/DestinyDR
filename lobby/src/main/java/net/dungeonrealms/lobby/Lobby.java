4c2cb1ddb860e381e3c890acfec7a9f920e6ac97
apollosoftware <contact@apollosoftware.io>
2016-07-11 20:16:01 -0400

    Added lobby module

---
+++ lobby/src/main/java/net/dungeonrealms/lobby/Lobby.java
@@ -0,0 +1,16 @@
+package net.dungeonrealms.lobby;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Class written by APOLLOSOFTWARE.IO on 7/11/2016
+ */
+public class Lobby extends JavaPlugin {
+
+
+    @Override
+    public void onEnable() {
+
+
+    }
+}
