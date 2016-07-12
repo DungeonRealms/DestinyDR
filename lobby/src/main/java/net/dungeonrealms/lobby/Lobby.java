f1792cc9dc13d05dd9852477d583601dbf65e97f
apollosoftware <contact@apollosoftware.io>
2016-07-11 22:19:31 -0400

    Changes

--- lobby/src/main/java/net/dungeonrealms/lobby/Lobby.java
+++ lobby/src/main/java/net/dungeonrealms/lobby/Lobby.java
@@ -2,11 +2,14 @@ package net.dungeonrealms.lobby;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 /**
  * Class written by APOLLOSOFTWARE.IO on 7/11/2016
  */
 public class Lobby extends JavaPlugin {
 
+    public static Logger log = Logger.getLogger("DungeonRealms");
 
     @Override
     public void onEnable() {
