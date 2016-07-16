211e611bd1949ac3fcbd6cff68d23a6cfc131c39
Chase <chasecool56@gmail.com>
2016-07-15 20:16:52 -0400

    Forgot a zero.

--- game/src/main/java/net/dungeonrealms/DungeonRealms.java
+++ game/src/main/java/net/dungeonrealms/DungeonRealms.java
@@ -470,7 +470,7 @@ public class DungeonRealms extends JavaPlugin {
 
 
         rebooterID = Bukkit.getScheduler().scheduleAsyncRepeatingTask(this, () -> {
-            if (System.currentTimeMillis() >= (SERVER_START_TIME + 1434000L)) {
+            if (System.currentTimeMillis() >= (SERVER_START_TIME + 14340000L)) {
                 scheduleRestartTask();
                 Bukkit.getScheduler().cancelTask(rebooterID);
             }
