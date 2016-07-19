2ec0b71a41961fc8cf3b53a637167dd9b0fcac59
Alan Lu <fearless2555@gmail.com>
2016-07-19 16:20:15 -0400

    Added broadcast command

---
+++ game/src/main/java/net/dungeonrealms/game/commands/CommandBroadcast.java
@@ -0,0 +1,29 @@
+package net.dungeonrealms.game.commands;
+
+import net.dungeonrealms.GameAPI;
+import net.dungeonrealms.game.database.DatabaseAPI;
+import net.dungeonrealms.game.database.player.Rank;
+import net.dungeonrealms.game.database.type.EnumData;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+/**
+ * Created by Alan Lu (dartaran) on 17-Jul-16.
+ */
+public class CommandBroadcast extends BasicCommand {
+
+    public CommandBroadcast(String command, String usage, String description, List<String> aliases) {
+        super(command, usage, description, aliases);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender instanceof Player && !Rank.isGM((Player) sender)) return true;
+        GameAPI.sendNetworkMessage("Broadcast", args[1]);
+        return false;
+    }
+}
