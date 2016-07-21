95909e068461e2284c725406190bd9abafb538a3
Proxying <kieran_quigley_CFC@hotmail.com>
2016-07-21 17:04:28 +0100

    Sync toggles.

--- game/src/main/java/net/dungeonrealms/game/mechanics/PlayerManager.java
+++ game/src/main/java/net/dungeonrealms/game/mechanics/PlayerManager.java
@@ -116,7 +116,7 @@ public class PlayerManager {
         }
 
         public void setToggleState(Player player, boolean state) {
-            DatabaseAPI.getInstance().update(player.getUniqueId(), EnumOperators.$SET, dbField, state, true, true);
+            DatabaseAPI.getInstance().update(player.getUniqueId(), EnumOperators.$SET, dbField, state, true, false);
             player.sendMessage((state ? ChatColor.GREEN : ChatColor.RED) + friendlyName + " - " + ChatColor.BOLD + (state ? "ENABLED" : "DISABLED"));
         }
     }
