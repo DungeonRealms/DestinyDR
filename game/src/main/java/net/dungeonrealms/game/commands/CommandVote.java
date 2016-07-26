3a0462f5366f38d902ab8449a38f2c9879ab6030
Alan Lu <fearless2555@gmail.com>
2016-07-25 22:20:40 -0400

    Added vote command

---
+++ game/src/main/java/net/dungeonrealms/game/commands/CommandVote.java
@@ -0,0 +1,32 @@
+package net.dungeonrealms.game.commands;
+
+import net.dungeonrealms.common.game.commands.BasicCommand;
+import net.dungeonrealms.common.game.database.DatabaseAPI;
+import net.dungeonrealms.common.game.database.player.rank.Rank;
+import net.dungeonrealms.common.game.database.type.EnumData;
+import net.dungeonrealms.game.player.json.JSONMessage;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.UUID;
+
+/**
+ * Created by Alan on 7/25/2016.
+ */
+public class CommandVote extends BasicCommand {
+    public CommandVote(String command, String usage, String description) {
+        super(command, usage, description);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender instanceof Player && !Rank.isGM((Player) sender)) return true;
+        final JSONMessage normal = new JSONMessage(ChatColor.AQUA + "To vote for 15 ECASH and 5% EXP click ");
+        normal.addURL(ChatColor.AQUA.toString() + ChatColor.BOLD + ChatColor.UNDERLINE + "HERE", ChatColor.AQUA, "http://minecraftservers.org/vote/174212");
+        normal.addText(ChatColor.AQUA + ". You can vote each day to get the rewards!");
+        return true;
+    }
+}
