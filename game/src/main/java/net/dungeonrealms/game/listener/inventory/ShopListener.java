8acf2fe37df29a09d8023ec797d428b7ff2b650c
apollosoftware <contact@apollosoftware.io>
2016-07-29 15:02:47 -0400

    Build #148 Moved database

--- game/src/main/java/net/dungeonrealms/game/listener/inventory/ShopListener.java
+++ game/src/main/java/net/dungeonrealms/game/listener/inventory/ShopListener.java
@@ -508,7 +508,7 @@ public class ShopListener implements Listener {
                         if (remainingStock > 0) {
                             itemClicked.setAmount(quantity);
                         } else {
-                            event.getInventory().setItem(event.getRawSlot(), new ItemStack(Material.AIR));
+                            event.getInventory().remove(event.getRawSlot());
                         }
                         DatabaseAPI.getInstance().update(shop.ownerUUID, EnumOperators.$INC, EnumData.GEMS, totalPrice, false);
                         if (shop.getOwner() != null) {
