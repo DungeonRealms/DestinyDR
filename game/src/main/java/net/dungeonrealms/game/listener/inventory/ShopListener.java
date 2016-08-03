c742eeeffe3ba411363652452d8287f4c5753cb9
Necrone <necroneunderscore@gmail.com>
2016-08-03 18:35:21 +0100

    Shop dupe fix

--- game/src/main/java/net/dungeonrealms/game/listener/inventory/ShopListener.java
+++ game/src/main/java/net/dungeonrealms/game/listener/inventory/ShopListener.java
@@ -459,7 +459,8 @@ public class ShopListener implements Listener {
                         clicker.sendMessage(ChatColor.RED + "No space available in inventory. Type 'cancel' or clear some room.");
                         return;
                     }
-                    if (!ShopMechanics.ALLSHOPS.containsKey(ownerName) || !shop.isopen) {
+                    if (!ShopMechanics.ALLSHOPS.containsKey(ownerName) || !shop.isopen ||
+                    		!(ShopMechanics.ALLSHOPS.get(ownerName).equals(shop))) {
                         clicker.sendMessage(ChatColor.RED + "The shop is no longer available.");
                         clicker.closeInventory();
                         return;
