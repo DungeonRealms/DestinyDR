80d1940ecbe519171caafeba862048a69877deb0
Necrone <necroneunderscore@gmail.com>
2016-08-05 04:42:23 +0100

    Gem display in bank properly fixed

--- game/src/main/java/net/dungeonrealms/game/listener/mechanic/BankListener.java
+++ game/src/main/java/net/dungeonrealms/game/listener/mechanic/BankListener.java
@@ -296,7 +296,8 @@ public class BankListener implements Listener {
                                 player.sendMessage(ChatColor.GREEN + ChatColor.BOLD.toString() + "+" + ChatColor.GREEN + size + ChatColor.BOLD + "G, New Balance: " + ChatColor.GREEN + newBalance + " GEM(s)");
                                 ItemStack bankItem = new ItemStack(Material.EMERALD);
                                 ItemMeta meta = bankItem.getItemMeta();
-                                meta.setDisplayName(ChatColor.GREEN + String.valueOf(getPlayerGems(player.getUniqueId()) + size) + ChatColor.BOLD.toString()
+                                int newBal = getPlayerGems(player.getUniqueId()) + size;
+                                meta.setDisplayName(ChatColor.GREEN + String.valueOf(newBal) + ChatColor.BOLD.toString()
                                         + ChatColor.GREEN + " GEM(s)");
                                 ArrayList<String> lore = new ArrayList<>();
                                 lore.add(ChatColor.GREEN + "Left Click " + ChatColor.GRAY + "to withdraw " + ChatColor.GREEN.toString() + ChatColor.BOLD + "RAW GEMS");
