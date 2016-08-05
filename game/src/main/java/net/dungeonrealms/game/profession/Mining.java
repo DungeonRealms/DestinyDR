11cc729b454249792d3d682bf308821d1d3aeb6e
Necrone <necroneunderscore@gmail.com>
2016-08-05 03:38:11 +0100

    Fixed minor visual bug with profession buff while mining

--- game/src/main/java/net/dungeonrealms/game/profession/Mining.java
+++ game/src/main/java/net/dungeonrealms/game/profession/Mining.java
@@ -280,7 +280,7 @@ public class Mining implements GenericMechanic {
         currentXP += experienceGain;
 
         if ((boolean) DatabaseAPI.getInstance().getData(EnumData.TOGGLE_DEBUG, p.getUniqueId())) {
-            p.sendMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + "          +" + ChatColor.YELLOW + experienceGain + ChatColor.BOLD + " EXP"
+            p.sendMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + "          +" + ChatColor.YELLOW + Math.round(experienceGain - professionBuffBonus) + ChatColor.BOLD + " EXP"
                     + ChatColor.YELLOW + ChatColor.GRAY + " [" + Math.round(currentXP - professionBuffBonus) + ChatColor.BOLD + "/" + ChatColor.GRAY + getEXPNeeded(getLvl(stackInHand)) + " EXP]");
 //            p.playSound(p.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1f, 1f);
             if (professionBuffBonus > 0) {
