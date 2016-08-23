6bff5934fc19572f8df059d6d05eeb774656ee49
Brad <brad.bayliss@blueyonder.co.uk>
2016-08-23 23:54:45 +0100

    Update to /list, you can now specify -m|-s|-h to refine the query. It also now shows the player rank.

--- game/src/main/java/net/dungeonrealms/game/command/CommandList.java
+++ game/src/main/java/net/dungeonrealms/game/command/CommandList.java
@@ -2,6 +2,7 @@ package net.dungeonrealms.game.command;
 
 import net.dungeonrealms.common.game.command.BaseCommand;
 import net.dungeonrealms.common.game.database.player.rank.Rank;
+import net.dungeonrealms.game.player.chat.GameChat;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -24,19 +25,51 @@ public class CommandList extends BaseCommand {
         if (commandSender instanceof ConsoleCommandSender || !Rank.isGM((Player) commandSender))
             return false;
 
+        if (args.length > 0 && (args[0].equals("-h") || args[0].equals("-help"))) {
+            commandSender.sendMessage(new String[] {
+                    ChatColor.YELLOW + ChatColor.BOLD.toString() + "Displays a list of online users: /list [-s|-m|-h]",
+                    ChatColor.YELLOW + "-s: Display only staff members.",
+                    ChatColor.YELLOW + "-m: Display only player moderators.",
+                    ChatColor.YELLOW + "-h: Display this help information."
+            });
+            return true;
+        }
+
         StringBuilder players = new StringBuilder();
 
+        final boolean staffOnly = args.length > 0 && args[0].equals("-s");
+        final boolean pmodsOnly = args.length > 0 && args[0].equals("-m");
+
+        String searchName = "Players";
+        if (staffOnly) {
+            searchName = "Staff Members";
+        } else if (pmodsOnly) {
+            searchName = "Player Moderators";
+        }
+
+        int onlinePlayers = 0; // Use an int that increments so we can refine the search.
         for (Player player : Bukkit.getOnlinePlayers()) {
+            // Searching for staff but player isn't staff, skip...
+            if (staffOnly && !Rank.isPMOD(player)) continue;
+            // Searching for mods only but player isn't a mod, skip...
+            if (pmodsOnly && !Rank.getInstance().getRank(player.getUniqueId()).equalsIgnoreCase("pmod")) continue;
+
+            onlinePlayers++;
             if (players.length() > 0) {
                 players.append(ChatColor.AQUA).append(", ").append(ChatColor.GOLD);
             }
-            players.append(player.getDisplayName());
+
+            // Format player name + rank properly.
+            String playerName = GameChat.getPreMessage(player);
+            players.append(playerName.substring(0, playerName.length() - 4));
         }
 
-        String onlinePlayers = Bukkit.getOnlinePlayers().size() + "";
-        commandSender.sendMessage(ChatColor.GREEN + "Players Online: " + ChatColor.LIGHT_PURPLE + onlinePlayers + ChatColor.GRAY + "/" + ChatColor.LIGHT_PURPLE + Bukkit.getMaxPlayers());
-        commandSender.sendMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + players.toString() + ChatColor.GRAY + "]");
-
+        if (players.length() > 0) {
+            commandSender.sendMessage(ChatColor.GREEN + ChatColor.BOLD.toString() + searchName + " Online: " + ChatColor.LIGHT_PURPLE + onlinePlayers + ChatColor.GRAY + "/" + ChatColor.LIGHT_PURPLE + Bukkit.getMaxPlayers());
+            commandSender.sendMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + players.toString() + ChatColor.GRAY + "]");
+        } else {
+            commandSender.sendMessage(ChatColor.RED + ChatColor.ITALIC.toString() + "No " + searchName.toLowerCase() + " online.");
+        }
         return true;
     }
 }
