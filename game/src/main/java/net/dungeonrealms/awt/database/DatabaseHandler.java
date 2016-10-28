ef2cccd59fb24906e6d4690ddb37a86101070aa7
VawkeIO <io.vawke@gmail.com>
2016-10-29 00:59:34 +0200

    Laying database framework.

---
+++ game/src/main/java/net/dungeonrealms/awt/database/DatabaseHandler.java
@@ -0,0 +1,18 @@
+package net.dungeonrealms.awt.database;
+
+import net.dungeonrealms.awt.SuperHandler;
+
+/**
+ * Created by Giovanni on 29-10-2016.
+ * <p>
+ * This file is part of the Dungeon Realms project.
+ * Copyright (c) 2016 Dungeon Realms;www.vawke.io / development@vawke.io
+ */
+public class DatabaseHandler implements SuperHandler.Handler
+{
+    @Override
+    public void prepare()
+    {
+
+    }
+}
---
+++ game/src/main/java/net/dungeonrealms/awt/database/sql/MySQL.java
@@ -0,0 +1,11 @@
+package net.dungeonrealms.awt.database.sql;
+
+/**
+ * Created by Giovanni on 29-10-2016.
+ * <p>
+ * This file is part of the Dungeon Realms project.
+ * Copyright (c) 2016 Dungeon Realms;www.vawke.io / development@vawke.io
+ */
+public class MySQL
+{
+}
