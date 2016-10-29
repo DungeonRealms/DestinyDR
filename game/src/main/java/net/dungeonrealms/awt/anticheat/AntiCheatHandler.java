5cbb5447516501e91a1a1b6d23c06e680a59c58e
Matthew <matt11matthew@gmail.com>
2016-10-29 10:49:13 -0400

    Nocheat

---
+++ game/src/main/java/net/dungeonrealms/awt/anticheat/AntiCheatHandler.java
@@ -0,0 +1,17 @@
+package net.dungeonrealms.awt.anticheat;
+
+import net.dungeonrealms.awt.SuperHandler;
+
+/**
+ * Copyright © 2016 Matthew E Development - All Rights Reserved
+ * You may NOT use, distribute and modify this code.
+ * <p>
+ * Created by Matthew E on 10/29/2016 at 10:41 AM.
+ */
+public class AntiCheatHandler implements SuperHandler.Handler {
+
+    @Override
+    public void prepare() {
+
+    }
+}
---
+++ game/src/main/java/net/dungeonrealms/awt/anticheat/Watch.java
@@ -0,0 +1,29 @@
+package net.dungeonrealms.awt.anticheat;
+
+import net.dungeonrealms.DungeonRealms;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+/**
+ * Created by Matthew E on 10/29/2016 at 10:42 AM.
+ */
+public abstract class Watch {
+
+    long checkTime;
+
+    public Watch(long checkTime) {
+        this.checkTime = checkTime;
+        if (this.checkTime == 0L) {
+            return;
+        }
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(DungeonRealms.getInstance(), () -> {
+            Bukkit.getOnlinePlayers().forEach(player -> check(player));
+        }, this.checkTime, this.checkTime);
+    }
+
+    public abstract void check(Player player);
+
+    public long getCheckTime() {
+        return checkTime;
+    }
+}
