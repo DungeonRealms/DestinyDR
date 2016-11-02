3721c3da9237c59b015d3189a5003fdf5921eae7
VawkeIO <io.vawke@gmail.com>
2016-11-02 20:11:01 +0100

    Object orientated attributes.

---
+++ game/src/main/java/net/dungeonrealms/vgame/item/weapon/attribute/WeaponAttribute.java
@@ -0,0 +1,20 @@
+package net.dungeonrealms.vgame.item.weapon.attribute;
+
+import lombok.Getter;
+
+/**
+ * Created by Giovanni on 2-11-2016.
+ * <p>
+ * This file is part of the Dungeon Realms project.
+ * Copyright (c) 2016 Dungeon Realms;www.vawke.io / development@vawke.io
+ */
+public class WeaponAttribute
+{
+    @Getter
+    private EnumWeaponAttribute weaponAttribute;
+
+    public WeaponAttribute()
+    {
+
+    }
+}
