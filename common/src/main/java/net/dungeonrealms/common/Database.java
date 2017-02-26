67a28768d74306fe1c664a778801f98e32bffd75
Brad <brad.bayliss@blueyonder.co.uk>
2017-02-26 23:33:35 +0000

    New DR user.

--- common/src/main/java/net/dungeonrealms/common/Database.java
+++ common/src/main/java/net/dungeonrealms/common/Database.java
@@ -2,7 +2,7 @@ package net.dungeonrealms.common;
 
 public enum Database {
 	DEV("dungeonrealms_event", "mongodb://dungeonrealms:sBFt%2BSk2r%5Ejw7%2BXZ%2B3%3Dm@ds157228-a0.mlab.com:57228,ds157228-a1.mlab.com:57228/dungeonrealms_event?replicaSet=rs-ds157228"),
-	NORMAL("dungeonrealms", "mongodb://dungeonrealms:ACNZuQdvjDrzWSqV@ds157228-a0.mlab.com:57228,ds157228-a1.mlab.com:57228/dungeonrealms?replicaSet=rs-ds157228");
+	NORMAL("dungeonrealms", "mongodb://druser:H%3A82F9xW3X425NrHra.UW7-gXV@ds157228-a0.mlab.com:57228,ds157228-a1.mlab.com:57228/dungeonrealms?replicaSet=rs-ds157228");
 	
 	private final String uri;
 	private final String dbName;
