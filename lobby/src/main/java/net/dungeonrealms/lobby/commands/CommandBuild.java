94ce2c9bde62ae49af8af7b398d9850d8fb29082
Brad <brad.bayliss@blueyonder.co.uk>
2017-04-04 21:41:21 +0100

    Use keyword instead of builder rank (due to trial builders).

--- lobby/src/main/java/net/dungeonrealms/lobby/commands/CommandBuild.java
+++ lobby/src/main/java/net/dungeonrealms/lobby/commands/CommandBuild.java
@@ -5,7 +5,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import net.dungeonrealms.common.game.command.BaseCommand;
-import net.dungeonrealms.common.game.database.player.rank.Rank;
 import net.dungeonrealms.common.network.bungeecord.BungeeUtils;
 import net.dungeonrealms.lobby.Lobby;
 import net.md_5.bungee.api.ChatColor;
@@ -18,12 +17,13 @@ public class CommandBuild extends BaseCommand {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (!(sender instanceof Player) || !Rank.isBuilder((Player)sender))
+		if (!(sender instanceof Player) || !Lobby.getInstance().isLoggedIn((Player)sender))
 			return false;
-		if (!Lobby.getInstance().isLoggedIn((Player)sender))
+
+		if (args.length == 0 || !args[0].equalsIgnoreCase("creative"))
 			return false;
-		
-		sender.sendMessage(ChatColor.GREEN + "Sending you to the build server...");
+
+		sender.sendMessage(ChatColor.YELLOW + ChatColor.ITALIC.toString() + "Attempting to send you to the build server...");
 		BungeeUtils.sendToServer(sender.getName(), "buildserver");
 		
 		return true;
