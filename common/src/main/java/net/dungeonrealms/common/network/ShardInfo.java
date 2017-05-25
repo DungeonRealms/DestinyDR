7291927f809e946ba0c725274ff263f3062f562d
Brad <brad.bayliss@blueyonder.co.uk>
2017-05-25 15:16:03 +0100

    Register new test shards on old build.

--- common/src/main/java/net/dungeonrealms/common/network/ShardInfo.java
+++ common/src/main/java/net/dungeonrealms/common/network/ShardInfo.java
@@ -30,8 +30,11 @@ public enum ShardInfo implements Serializable {
     SUB1("SUB-1", "sub1", new ServerAddress("158.69.121.67", 42001)),
 
     // CS 1 SHARD //
-    CS1("CS-1", "cs1", new ServerAddress("158.69.121.48", 45521));
+    CS1("CS-1", "cs1", new ServerAddress("158.69.121.48", 45521)),
 
+    // -- NEW SERVER TEST SHARDS -- //
+    TESTUS0("TEST-US0", "testus0", new ServerAddress("158.69.121.40", 40012)),
+    TEST("TEST", "test", new ServerAddress("158.69.121.40", 40013));
 
 
     @Getter
