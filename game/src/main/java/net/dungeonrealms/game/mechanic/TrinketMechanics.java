98e6ffd998680c189ea815e98971704979361241
iFamasssxD <dardanrocks97@gmail.com>
2017-06-16 19:27:12 -0400

    Remove master check for trinkets to work..

--- game/src/main/java/net/dungeonrealms/game/mechanic/TrinketMechanics.java
+++ game/src/main/java/net/dungeonrealms/game/mechanic/TrinketMechanics.java
@@ -1,7 +1,6 @@
 package net.dungeonrealms.game.mechanic;
 
 import net.dungeonrealms.DungeonRealms;
-import net.dungeonrealms.common.network.ShardInfo;
 import net.dungeonrealms.database.PlayerWrapper;
 import net.dungeonrealms.game.item.ItemType;
 import net.dungeonrealms.game.item.items.functional.accessories.Trinket;
@@ -31,37 +30,35 @@ public class TrinketMechanics implements GenericMechanic, Listener {
 
     @Override
     public void startInitialization() {
-        if (DungeonRealms.isMaster() || DungeonRealms.getShard().getType() == ShardInfo.ShardType.DEVELOPMENT) {
-            Bukkit.getPluginManager().registerEvents(this, DungeonRealms.getInstance());
-            Bukkit.getScheduler().scheduleSyncRepeatingTask(DungeonRealms.getInstance(), () -> {
-                for (PlayerWrapper wrapper : PlayerWrapper.getPlayerWrappers().values()) {
-                    if (wrapper.getPlayer() == null || !wrapper.getPlayer().isOnline()) continue;
-                    Player player = wrapper.getPlayer();
-                    TrinketItem item = Trinket.getActiveTrinketItem(player);
-                    TrinketItem tItem = lastTrinketItem.get(player.getUniqueId());
-                    boolean needsUpdating = false;
-                    if (item != null) {
-                        if (tItem != null && tItem.equals(item)) //No change
-                            continue;
+        Bukkit.getPluginManager().registerEvents(this, DungeonRealms.getInstance());
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(DungeonRealms.getInstance(), () -> {
+            for (PlayerWrapper wrapper : PlayerWrapper.getPlayerWrappers().values()) {
+                if (wrapper.getPlayer() == null || !wrapper.getPlayer().isOnline()) continue;
+                Player player = wrapper.getPlayer();
+                TrinketItem item = Trinket.getActiveTrinketItem(player);
+                TrinketItem tItem = lastTrinketItem.get(player.getUniqueId());
+                boolean needsUpdating = false;
+                if (item != null) {
+                    if (tItem != null && tItem.equals(item)) //No change
+                        continue;
 
-                        player.playSound(player.getLocation(), Sound.ENTITY_HORSE_SADDLE, 5, 1.1F);
-                        this.lastTrinketItem.put(player.getUniqueId(), item);
-                        player.sendMessage(ChatColor.YELLOW.toString() + ChatColor.BOLD + (tItem != null ? tItem.getDisplayName() : "Nothing") + " " + ChatColor.GRAY + "-> " + ChatColor.WHITE + item.getDisplayName());
-                        needsUpdating = true;
-                    } else if (tItem != null) {
-                        //No more item in this slot?
-                        player.playSound(player.getLocation(), Sound.ENTITY_ARMORSTAND_BREAK, 5, 1.6F);
-                        player.sendMessage(ChatColor.YELLOW.toString() + ChatColor.BOLD + tItem.getDisplayName() + " " + ChatColor.GRAY + "-> " + ChatColor.GRAY + "Nothing");
-                        this.lastTrinketItem.remove(player.getUniqueId());
-                        needsUpdating = true;
-                    }
-
-                    if (needsUpdating)
-                        wrapper.calculateAllAttributes();
-
+                    player.playSound(player.getLocation(), Sound.ENTITY_HORSE_SADDLE, 5, 1.1F);
+                    this.lastTrinketItem.put(player.getUniqueId(), item);
+                    player.sendMessage(ChatColor.YELLOW.toString() + ChatColor.BOLD + (tItem != null ? tItem.getDisplayName() : "Nothing") + " " + ChatColor.GRAY + "-> " + ChatColor.WHITE + item.getDisplayName());
+                    needsUpdating = true;
+                } else if (tItem != null) {
+                    //No more item in this slot?
+                    player.playSound(player.getLocation(), Sound.ENTITY_ARMORSTAND_BREAK, 5, 1.6F);
+                    player.sendMessage(ChatColor.YELLOW.toString() + ChatColor.BOLD + tItem.getDisplayName() + " " + ChatColor.GRAY + "-> " + ChatColor.GRAY + "Nothing");
+                    this.lastTrinketItem.remove(player.getUniqueId());
+                    needsUpdating = true;
                 }
-            }, 20, 20);
-        }
+
+                if (needsUpdating)
+                    wrapper.calculateAllAttributes();
+
+            }
+        }, 20, 20);
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
