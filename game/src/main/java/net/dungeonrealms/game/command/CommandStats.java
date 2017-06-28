94233760293fa7fbfef092f99001393d5361d32b
iFamasssxD <dardanrocks97@gmail.com>
2017-06-28 10:14:55 -0400

    Re-add /stats

---
+++ game/src/main/java/net/dungeonrealms/game/command/CommandStats.java
@@ -0,0 +1,30 @@
+package net.dungeonrealms.game.command;
+
+import net.dungeonrealms.common.game.command.BaseCommand;
+import net.dungeonrealms.game.player.inventory.menus.guis.PlayerProfileGUI;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class CommandStats extends BaseCommand {
+
+    public CommandStats(String command, String usage, String description, List<String> aliases) {
+        super(command, usage, description, aliases);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender s, Command cmd, String string, String[] args) {
+        if (s instanceof ConsoleCommandSender)
+            return false;
+
+        Player player = (Player) s;
+        player.sendMessage(ChatColor.GRAY + "The stats page has been merged with the profile page!");
+        player.sendMessage(ChatColor.GRAY + "Please use /profile to access this menu in the future!");
+        new PlayerProfileGUI(player).open(player, null);
+        return true;
+    }
+}
