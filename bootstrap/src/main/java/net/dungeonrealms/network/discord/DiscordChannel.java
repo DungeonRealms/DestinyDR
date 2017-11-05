053f0afe3fbd47174b875f307d7db23f0fa02a51
Kneesnap <davidmenlo2@gmail.com>
2017-11-05 14:49:40 -0800

    Update webhooks.

--- bootstrap/src/main/java/net/dungeonrealms/network/discord/DiscordChannel.java
+++ bootstrap/src/main/java/net/dungeonrealms/network/discord/DiscordChannel.java
@@ -15,10 +15,10 @@ import com.google.gson.JsonObject;
  */
 @AllArgsConstructor @Getter
 public enum DiscordChannel {
-	DR_DISCUSSION("Dungeon Realms", "http://files.enjin.com/631183/portal/drlogo.png", "rnPUhz2zttpfkX0gvM0kNzgcH7KDfOcOCA0KrQg78hB6Z4nOXmhnukx0bcwqAo49I4nL", "290594073587351554", null),
-	DEVELOPMENT("Neo", "http://i.imgur.com/KyGcQRt.png", "Wh3y8iKeY33M-2kWHdQx00I6HPaCqOB7VC3Pt5qy8wODIFTswIOZrTWnkbauQP57mWwT", "287277718482059264", PlayerRank.DEV),
-	NOTIFICATIONS("Officer Johnson", "http://i.imgur.com/8MW7apB.png", "pdOLstfvB43aimMizGQfFjTXgmEkqNdy-IeaF2jmAWIlgwxT0M1a_h8ShIizM3GHeqjP", "287302942392844288", PlayerRank.GM),
-	STAFF_REPORTS("Agent 47", "http://i.imgur.com/x8I5fWc.png", "L3_C9yNHuyOWWSNT1d_qokXt53ymftforgMwabLwK75dmiu8S0bYwZjnfQsYEuWJvfly", "290444027344584704", null);
+	DR_DISCUSSION("Dungeon Realms", "http://files.enjin.com/631183/portal/drlogo.png", "gavZCzsbod42Y08vvhEd7LNiUFFSAHAXX92bzIoycnvVDK2LQqSFl_gE-z-2kwoQE0y9", "376849765205278732", null),
+	DEVELOPMENT("Neo", "http://i.imgur.com/KyGcQRt.png", "vGxxMGAC_h7XvxU54sKoseJNxGVxgUBhrNCJ9B0mq-J-pD7s3dR2HWH7A8vCyuUUGG2i", "376849120461324298", PlayerRank.DEV),
+	NOTIFICATIONS("Officer Johnson", "http://i.imgur.com/8MW7apB.png", "4eS4fHzYFqB7riHQeOmASDazQigrm9e_JB5p6Eg6Bt5EzMkaCZF4cnHCdo4l7H6v96pe", "376849631222562820", PlayerRank.GM),
+	STAFF_REPORTS("Agent 47", "http://i.imgur.com/x8I5fWc.png", "4ZUlK7P1w-pUnbOIftR4yZW1vgRvY4QRtX3vfK-WVR8CITxvpOfwehrz2QCZQtCZl8cw", "376849708032720897", null);
 
 	private final String username;
 	private final String avatarURL;
